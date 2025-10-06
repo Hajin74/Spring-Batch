f600d42a605bcd48e088a6f939b8659cb2401856
Hajin74 <qwertygoov@naver.com>
2025-10-06 15:41:36 +0900

    오래된 데이터베이스 삭제 Tasklet

--- src/main/java/com/system/batch/FileCleanupBatchConfig.java
+++ src/main/java/com/system/batch/FileCleanupBatchConfig.java
@@ -6,10 +6,14 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 public class FileCleanupBatchConfig {
 
@@ -37,6 +41,26 @@ public class FileCleanupBatchConfig {
     public Job deleteOldFilesJob() {
         return new JobBuilder("deleteOldFilesJob", jobRepository)
             .start(deleteOldFilesStep())
+            .build();
+    }
+
+    @Bean
+    public Step deleteOldRecordsStep() {
+        // 간단한 작업이라 람다식으로 구현
+        return new StepBuilder("deleteOldRecordsStep", jobRepository)
+            .tasklet((contribution, chunkContext) -> {
+                //int deleted = jdbcTemplete.update("DELETE FROM logs WHERE created < NOW() - INTERNAL 7 DAY");
+                //log.info("===== {}개의 오래된 레코드가 삭제되었습니다. =====", deleted);
+                log.info("===== 오래된 레코드가 삭제되었습니다. =====");
+                return RepeatStatus.FINISHED;
+            }, transactionManager)
+            .build();
+    }
+
+    @Bean
+    public Job deleteOldRecordsJob() {
+        return new JobBuilder("deleteOldRecordsJob", jobRepository)
+            .start(deleteOldRecordsStep()) // Step을 Job에 등록
             .build();
     }
 }
